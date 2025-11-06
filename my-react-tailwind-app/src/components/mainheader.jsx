22ba7b66d6b3be79ce6d224df760263eb2cc5c33
PatrickArlan <patrickarlanbrequillo@gmail.com>
2025-11-06 21:39:02 +0800

    updated

--- my-react-tailwind-app/src/components/mainheader.jsx
+++ my-react-tailwind-app/src/components/mainheader.jsx
@@ -1,8 +1,8 @@
 export default function MainHeader() {
   return (
     <header className="site-header py-4 px-10 flex justify-between items-center shadow-md flex-wrap w-full">
-      <h1 className="text-2xl md:text-3xl font-bold">Patrick Brequillo</h1>
-      <nav className=" site-nav space-x-6 mt-2 md:mt-0 text-sm md:text-base">
+      <h1 className="text-2xl md:text-3xl font-bold">EXPT: Portfolio</h1>
+      <nav className=" site-nav space-x-6 mt-2 mx-6 md:mt-0 text-sm md:text-base">
         <a href="#about" className="hover:text-gray-200">About</a>
         <a href="#projects" className="hover:text-gray-200">Projects</a>
         <a href="#contact" className="hover:text-gray-200">Contact</a>
